035a977bf2bbb08d7f690cba5ae912c3a4b5b856
RudyWade1 <104886375+RudyWade1@users.noreply.github.com>
2024-02-27 17:15:14 +0300

    123

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -5,22 +5,25 @@ public class Shooting : MonoBehaviour
 {
     [SerializeField] private float _force;
     [SerializeField] private float _shootingTime;
-    [SerializeField] private GameObject _bulletPrefab;
+    [SerializeField] private Bullet _bulletPrefab;
     [SerializeField] private Transform _shootTatget;
+
+    private WaitForSeconds _waitForSeconds;
 
     private void Start()
     {
+        _waitForSeconds = new WaitForSeconds(_shootingTime);
         StartCoroutine(_shootingWorker());
     }
 
     private IEnumerator _shootingWorker()
     {
         Vector3 bulletDirection = (_shootTatget.position - transform.position).normalized;
-        GameObject bullet = Instantiate(_bulletPrefab, transform.position + bulletDirection, Quaternion.identity);
+        Bullet bullet = Instantiate(_bulletPrefab, transform.position + bulletDirection, Quaternion.identity);
 
         bullet.GetComponent<Rigidbody2D>().velocity = bulletDirection * _force;
 
-        yield return new WaitForSeconds(_shootingTime);
+        yield return _waitForSeconds;
         StartCoroutine(_shootingWorker());
     }
 }
